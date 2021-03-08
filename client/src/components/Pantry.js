437c790bf360d77fdf1ae89c30006b7e25f92be7
Eli Cusic <tntitans31@gmail.com>
2021-03-08 14:14:38 -0600

    set up request for getting pantry ingredients

--- client/src/components/Pantry.js
+++ client/src/components/Pantry.js
@@ -2,10 +2,31 @@ import React, { Component } from 'react';
 
 class Pantry extends Component {
 
+    constructor(){
+        super()
+        this.state = {
+            ingredients: []
+        }
+      }
+    
+      componentDidMount(){
+        fetch(`http://localhost:3001/users/1/pantries/ingredients`)
+        .then(resp =>  resp.json())
+        .then(ingredients => {
+          console.log(ingredients)
+        //   this.setState({
+        //     ingredients: ingredients
+        //   })
+        })
+      }
+
   render() {
     return (
     <div>
-        Pantry
+        <h2>Pantry: </h2>
+        <ul>
+            { this.state.ingredients.map(ing => <li>{ing.name}</li>) }
+        </ul>
     </div>
     );
   }
--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -5,10 +5,12 @@ class UserProfile extends Component {
   render() {
     return (
     <div>
-        User Profile Component
-        <p onClick={this.props.showPantry}>Pantry |</p>
-        <p onClick={this.props.showRecipes}>| Recipes |</p>
-        <p onClick={this.props.newRecipe}>| Create New Recipe</p>
+        <ul>
+            <li onClick={this.props.showPantry}>Pantry </li>
+            <li onClick={this.props.showRecipes}>Recipes </li>
+            <li onClick={this.props.newRecipe}>Create New Recipe </li>
+        </ul>
+       
     </div>
     );
   }
