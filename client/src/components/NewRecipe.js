83ca63d52261919f0559bc4c50410a2cfa5457ce
Eli Cusic <tntitans31@gmail.com>
2021-03-09 09:21:33 -0600

    set up post fetch to send new recipe

--- client/src/components/NewRecipe.js
+++ client/src/components/NewRecipe.js
@@ -2,15 +2,45 @@ import React, { Component } from 'react';
 
 class NewRecipe extends Component {
 
+  sendRecipe (recipe) {
+      const configObject = {
+          method: 'POST',
+          headers: {
+              "Content-Type": 'application/json',
+              "Accept": 'application/json'
+          },
+          body: JSON.stringify(recipe)
+      }
+
+      fetch(`http://localhost:3000/users/1/recipes`, configObject)
+          .then(function(response){
+              return response.json()
+          })
+          .then(function(json){
+              // do I need to do something with the data here ??
+            console.log(json)
+            console.log("should I do something here??")
+          })
+          // add catch
+  }
+
+  
+
   render() {
     return (
     <div>
         New Recipe:
         <form>
-          <select name="ingredients" id="new-recipe-ingredients">
-            { this.props.ingredients.map(ingredient => <option value={ ingredient.name }>{ ingredient.name }</option>)}
-          </select>
-          <input type ="number"></input>  
+          <div id="new-recipe-ingredients">
+            <select name="ingredients" id="new-recipe-ingredients">
+                { this.props.ingredients.map(ingredient => <option placeholder="ingredient" value={ ingredient.name }>{ ingredient.name }</option>)}
+            </select>
+            <input type ="number"></input>
+            <input type ="text" placeholder="unit"></input> <br></br>
+          </div>
+          
+
+          <p>++ Add Ingredient ++</p>
         </form>
     </div>
     );
