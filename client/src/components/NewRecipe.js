7494d57f6fd2025ae8db99d153907222b633a6c7
Eli Cusic <tntitans31@gmail.com>
2021-03-09 10:17:45 -0600

    added state handling to new recipe form

--- client/src/components/NewRecipe.js
+++ client/src/components/NewRecipe.js
@@ -3,10 +3,16 @@ import NewIngredient from './NewIngredient'
 
 class NewRecipe extends Component {
 
+  // STANDARDIZE FORM TO HANDLE EDIT RECIPE AS WELL
+
   constructor(){
     super()
     this.state = {
-      newIngredients: []
+      newIngredients: [],
+      recipeName: "",
+      recipeServings: 0,
+      recipeIngredients: [],
+      recipeInstructions: ""
     }
   }
 
@@ -16,25 +22,28 @@ class NewRecipe extends Component {
     })
   }
 
-  sendRecipe = (recipe) => {
-      const configObject = {
-          method: 'POST',
-          headers: {
-              "Content-Type": 'application/json',
-              "Accept": 'application/json'
-          },
-          body: JSON.stringify(recipe)
-      }
+  sendRecipe = (event) => {
+    event.preventDefault()
+    console.log("send recipe")
+    console.log(this.state)
+      // const configObject = {
+      //     method: 'POST',
+      //     headers: {
+      //         "Content-Type": 'application/json',
+      //         "Accept": 'application/json'
+      //     },
+      //     body: JSON.stringify(recipe)
+      // }
 
-      fetch(`http://localhost:3000/users/1/recipes`, configObject)
-          .then(function(response){
-              return response.json()
-          })
-          .then(function(json){
-              // do I need to do something with the data here ??
-            console.log(json)
-            console.log("should I do something here??")
-          })
+      // fetch(`http://localhost:3000/users/1/recipes`, configObject)
+      //     .then(function(response){
+      //         return response.json()
+      //     })
+      //     .then(function(json){
+      //         // do I need to do something with the data here ??
+      //       console.log(json)
+      //       console.log("should I do something here??")
+      //     })
           // add catch
   }
 
@@ -46,20 +55,52 @@ class NewRecipe extends Component {
     })
   }
 
+  changeName = event => {
+    this.setState({
+      recipeName: event.target.value
+    })
+  }
+
+  changeServings = event => {
+    this.setState({
+      recipeServings: event.target.value
+    })
+  }
+
+  changeInstructions = event => {
+    this.setState({
+      recipeInstructions: event.target.value
+    })
+  }
+
+  changeIngredients = event => {
+    obj = event.target.value
+    console.log(obj)
+    // this.setState({
+    //   recipeIngredients: event.target.value
+    // })
+  }
+
   render() {
     return (
     <div>
         <h2>New Recipe:</h2><br></br> 
-        <form>
-          Recipe Name: <input type ="text"></input> <br></br> <br></br>
-          Servings: <input type ="number"></input>  <br></br> <br></br>
+        <form onSubmit={event => this.sendRecipe(event)}>
+          
+          Recipe Name: <input type ="text" onChange={event => this.changeName(event)}></input> <br></br> <br></br>
+          
+          Servings: <input type ="number" onChange={event => this.changeServings(event)}></input>  <br></br> <br></br>
+          
           {/* set ingredients list up as a table??? */}
           <div id="new-recipe-ingredients">
             { this.state.newIngredients.map(ing => ing) }
           </div>
+          
           <p onClick={this.addIngredient}>++ Add Ingredient ++</p> <br></br> 
-          Instructions: <input type="text"></input> <br></br> <br></br>
-          <input type="submit" onClick={ this.sendRecipe }></input>
+          
+          Instructions: <input type="text" onChange={event => this.changeInstructions(event)}></input> <br></br> <br></br>
+          
+          <input type="submit"></input>
         </form>
     </div>
     );
