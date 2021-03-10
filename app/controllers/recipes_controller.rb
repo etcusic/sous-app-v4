5c86e9b95b35f2f78032e929f8e2ce34bf4f4002
Eli Cusic <tntitans31@gmail.com>
2021-03-10 07:47:07 -0600

    got params to come through for creating a new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,17 +11,20 @@ class RecipesController < ApplicationController
     end
 
     def create
+        hash = recipe_params
+        hash[:ingredients] = ingredient_params
+        # @recipe = Recipe.create(recipe_params)
         binding.pry
     end
 
     private
 
     def recipe_params
-        params.permit(:user_id, :name, :servings, :instructions, :ingredients, :quantity, :unit)
+        params.permit(:user_id, :name, :servings, :instructions)
     end
 
-    def ingredient_params
-        params[:ingredients] .map{params.permit(:name, :quantity, :unit)}
+    def ingredient_params 
+        params[:ingredients] .map{|x| {name: x[:name], quantity: x[:quantity], unit: x[:unit]}}
     end
 
 end
