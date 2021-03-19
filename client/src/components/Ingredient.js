2eb8394278846c137b2812b32b4090043bae29d7
Eli Cusic <tntitans31@gmail.com>
2021-03-19 10:00:59 -0500

    added category to narrow selection of ingredients in form

--- client/src/components/Ingredient.js
+++ client/src/components/Ingredient.js
@@ -2,11 +2,59 @@ import React, { Component } from 'react';
 
 class Ingredient extends Component {
 
+    constructor(){
+        super()
+        this.state = {
+            pantry: {},
+            ingredients: []
+        }
+    }
+
+    componentDidMount(){
+        console.log(this.props.pantry.map(x => x.category))
+        const pantry = {
+            all: this.props.pantry,
+            proteins: this.props.pantry.filter(x => x.category === "proteins"),
+            driedGoods: this.props.pantry.filter(x => x.category === "dried goods"),
+            produce: this.props.pantry.filter(x => x.category === "produce"),
+            dairy: this.props.pantry.filter(x => x.category === "dairy"),
+            frozenGoods: this.props.pantry.filter(x => x.category === "frozen goods"),
+            condiments: this.props.pantry.filter(x => x.category === "condiments"),
+            spices: this.props.pantry.filter(x => x.category === "spices")
+        }
+        this.setState({
+            pantry: pantry,
+            ingredients: pantry["all"]
+        })
+    }
+
+    showCategory = (event) => {
+        // event.preventDefault()
+        let ingredientsByCategory = [...this.state.pantry[event.target.value]]
+        this.setState({
+            ingredients: ingredientsByCategory
+        })
+    }
+
   render() {
     return (
     <div key={`new-ingredient-${this.props.keyId}`}>
         Ingredient: 
-        <select name="ingredients" id="new-recipe-ingredients" onChange={event => this.props.changeIngredient(event, this.props.keyId, "id") }>
+        <select onChange={ event => this.showCategory(event) }>
+            <option key="category-1" value="all">all</option>
+            <option key="category-3" value="proteins">proteins</option>
+            <option key="category-5" value="dried goods">dried goods</option>
+            <option key="category-2" value="produce">produce</option>
+            <option key="category-4" value="dairy">dairy</option>
+            <option key="category-6" value="frozen goods">frozen goods</option>
+            <option key="category-7" value="condiments">condiments</option>
+            <option key="category-8" value="spices">spices</option>
+        </select>
+        <select>
+            <option>- ingredients -</option>
+            { this.state.ingredients.map(ing => <option value={ing.id}>{ing.name}</option>) }
+        </select>
+        {/* <select name="ingredients" id="new-recipe-ingredients" onChange={event => this.props.changeIngredient(event, this.props.keyId, "id") }>
             { this.props.pantry.map(ingredient => {
                 if (ingredient.id == this.props.ingredientId){
                     return <option selected key={`ingredient-option-${ingredient.id}`} value={ ingredient.id } >{ `${ingredient.name} - ${ingredient.unit}` }</option>
@@ -14,7 +62,7 @@ class Ingredient extends Component {
                     return <option key={`ingredient-option-${ingredient.id}`} value={ ingredient.id }>{ `${ingredient.name} - ${ingredient.unit}` }</option>
                 }
             })}
-        </select>
+        </select> */}
         <input type ="number" value={ this.props.ingredientQuantity } onChange={ event => this.props.changeIngredient(event, this.props.keyId, "quantity") }></input>
         <button onClick={ event => this.props.removeIngredient(event, this.props.keyId) }>- remove -</button>
     </div>
--- client/src/components/RecipeForm.js
+++ client/src/components/RecipeForm.js
@@ -7,45 +7,36 @@ class RecipeForm extends Component {
   constructor(){
     super()
     this.state = {
-      ingredientComponents: [],
-      recipe: {
         id: 0,
         name: "",
         servings: 0,
         instructions: "",
-        ingredients: []
-      }
-        
+        ingredients: []        
     }
   }
 
   // fix state throughout this component!
   componentDidMount(){
-    console.log(this.props.recipe)
       this.setState({
-        ingredientComponents: this.props.recipe.ingredients.map((ing, i) => this.makeComponent(ing, i)),
-        recipe: {
           id: this.props.recipe.id,
           name: this.props.recipe.name,
           servings: this.props.recipe.servings,
           instructions: this.props.recipe.instructions,
           ingredients: this.props.recipe.ingredients
-        }
       })
     } 
 
-    // revisit add and remove ingredients - needs to be added and removed from both ingredientComponents and recipe.ingredients
   addIngredient = () => {
     let newState = Object.assign({}, this.state)
-    const emptyIngredient = {id: 0, name: "", quantity: 0, unit: ""}
-    newState.ingredientComponents.push(this.makeComponent(emptyIngredient, newState.ingredientComponents.length - 1))
-    newState.recipe.ingredients.push(emptyIngredient)
+    newState.ingredients.push({id: 0, name: "", quantity: 0, unit: ""})
     this.setState( newState )
   }
 
     removeIngredient = (event, keyId) => {
         event.preventDefault()
-        // needs to be fixed - I'll need to store ingredient components in state and map that instead. Which means reincorporating recipe sub-object
+        let newState = Object.assign({}, this.state)
+        newState.ingredients = newState.ingredients.filter((ing, i) => i !== keyId - 1)
+        this.setState( newState )
     }
 
     makeComponent = (ing, i) => {
@@ -63,36 +54,41 @@ class RecipeForm extends Component {
     changeRecipe = (event, category) => {
       console.log(this.state.recipe)
         let newState = Object.assign({}, this.state)
-        newState.recipe[category] = event.target.value
+        newState[category] = event.target.value
         this.setState(newState)
     }
 
     changeIngredient = (event, id, category) => {
         let newState = Object.assign({}, this.state)
-        newState.recipe.ingredients[id - 1][category] = event.target.value
+        newState.ingredients[id - 1][category] = event.target.value
         this.setState( newState )
+    }
+
+    checkIngredients = () => {
+      console.log(this.state)
     }
 
   render() {
     return (
     <div>
         <h2>New Recipe:</h2><br></br> 
-        <form onSubmit={event => sendRecipeData(event, this.props.routeMethod, this.state.recipe, this.props.userId, this.props.showRecipe)}>
+        <form onSubmit={event => sendRecipeData(event, this.props.routeMethod, this.state, this.props.userId, this.props.showRecipe)}>
           
-          Recipe Name: <input type ="text" value={ this.state.recipe.name } onChange={event => this.changeRecipe(event, "name")}></input> <br></br> <br></br>
+          Recipe Name: <input type ="text" value={ this.state.name } onChange={event => this.changeRecipe(event, "name")}></input> <br></br> <br></br>
           
-          Servings: <input type="number" value={ this.state.recipe.servings } onChange={event => this.changeRecipe(event, "servings")}></input>  <br></br> <br></br>
+          Servings: <input type="number" value={ this.state.servings } onChange={event => this.changeRecipe(event, "servings")}></input>  <br></br> <br></br>
           
             <div id="new-recipe-ingredients">
-                { this.state.recipe.ingredients.map((ing, i) => this.makeComponent(ing, i)) }
+                { this.state.ingredients.map((ing, i) => this.makeComponent(ing, i)) }
             </div>
           
           <p onClick={ this.addIngredient }>++ Add Ingredient ++</p> <br></br> 
           
-          Instructions: <input type="text" value={ this.state.recipe.instructions } onChange={event => this.changeRecipe(event, "instructions")}></input> <br></br> <br></br>
+          Instructions: <input type="text" value={ this.state.instructions } onChange={event => this.changeRecipe(event, "instructions")}></input> <br></br> <br></br>
           
           <input type="submit"></input>
         </form>
+        <button onClick={ this.checkIngredients }>Check 1,2</button>
     </div>
     );
   }
