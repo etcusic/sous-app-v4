f970c76464700c69684592dce41b2a89233c1262
Eli Cusic <tntitans31@gmail.com>
2021-03-25 13:54:41 -0500

    set up form to inherit from existing meals

--- client/src/components/WeeklyMenuForm.js
+++ client/src/components/WeeklyMenuForm.js
@@ -8,7 +8,8 @@ class WeeklyMenuForm extends Component {
         this.state = {
             weeklyMenuId: 0,
             dailyMenus: [],
-            recipes: []
+            recipes: [],
+            mealCategories: ["--", "breakfast", "lunch", "dinner"]
         }
     }
 
@@ -17,68 +18,83 @@ class WeeklyMenuForm extends Component {
         fetch(`http://localhost:3001/users/${this.props.userId}/recipes`)
         .then(resp =>  resp.json())
         .then(recipes => {
-            console.log(this.props.weeklyMenu)
+            let obj = {0: "--------"}
+            recipes.forEach(recipe => obj[recipe.id] = recipe.name)
             this.setState({
                 weeklyMenuId: this.props.weeklyMenu.id,
-                dailyMenus: this.props.weeklyMenu.dailyMenus,
-                recipes: recipes
+                dailyMenus: this.props.weeklyMenu.daily_menus,
+                recipes: obj
             })
         })
     }
 
-    changeMenu = (event, index, cat) => {
-        let menu = [...this.state.weeklyMenu]
-        menu[index][cat] = event.target.value
+    changeMenu = (event, i, j, cat) => {
+        let menu = [...this.state.dailyMenus]
+        console.log(menu[i].meals[j])
+        menu[i].meals[j][cat] = event.target.value
         this.setState({
-            weeklyMenu: menu
+            dailyMenus: menu
         })
     }
 
     checkState = () => {
-        console.log(this.state.weeklyMenu)
+        console.log(this.state)
     }
 
   render() {
     return (
     <div>
         <h2>Edit Weekly Menu:</h2>
-        {/* <form onSubmit={ event => sendWeeklyMenu(event, this.props.userId, this.props.method, this.state.weeklyMenu, this.props.updateMenu) }>
+        {/* <form onSubmit={ event => sendWeeklyMenu(event, this.props.userId, this.props.method, this.state.weeklyMenu, this.props.updateMenu) }> */}
+        <form>
             <table>
                 <thead>
                     <tr>
                         <th>Day:</th>
+                        <th>Meal:</th>
                         <th>Dish:</th>
                         <th>Quantity:</th>
                     </tr>
                 </thead>
                 <tbody>
-                    { this.state.weeklyMenu.map((obj, index) => {
+                    { this.state.dailyMenus.map((menu, i) => menu.meals.map((meal, j) => {
                         return (
                             <tr>
-                                <td>{ obj.day }</td>
-                                <td>
-                                    <select onChange={ event => this.changeMenu(event, index, "name") }>
-                                        <option value="invalid">-------</option>
-                                        { this.state.recipes.map((recipe, i) => {
-                                            if (recipe.name === obj.name){
-                                                return <option selected value={ recipe.name }>{ recipe.name }</option>
+                                <td>{ menu.date }</td>
+                                <td> 
+                                    <select>
+                                        { this.state.mealCategories.map(cat => {
+                                            if (cat === meal.category) {
+                                                return <option selected >{ cat }</option>
                                             } else {
-                                                return <option value={ recipe.name }>{ recipe.name }</option>
+                                                return <option>{ cat }</option>
                                             }
-                                        }) }
+                                        })}
                                     </select>
                                 </td>
-                                <td><input type ="number" value={ obj.quantity } onChange={ event => this.changeMenu(event, index, "quantity")}></input></td>
+                                <td> 
+                                    <select>
+                                        { Object.keys(this.state.recipes).map(key => {
+                                            if (key === meal.recipe_id) {
+                                                return <option selected >{ this.state.recipes[key] }</option>
+                                            } else {
+                                                return <option>{ this.state.recipes[key] }</option>
+                                            }
+                                        })}
+                                    </select>
+                                </td>
+                                <td><input type="number" value={ meal.quantity }></input></td>
                             </tr>
                         )
-                    })}
+                    })) }
                 </tbody>
             </table>
-            <button onClick={ this.checkState }>Check State</button>
             <br></br><br></br>
             <input type="submit"></input>
-        </form> */}
+        </form> 
         
+        <br></br><br></br>
+        <button onClick={ this.checkState }>Check State</button>
     </div>
     );
   }
